d3a7c6cedbe6cad604b124b04b4aba1bfb00c36e
matthew gregory <matthew.gregory@lmh.ox.ac.uk>
2014-07-27 11:35:13 +0100

    R script to inverse a matrix modified from coursera cachevector code. Final version with example at the bottom hashtagged.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## An example of cacheing time consuming function results
+## as part of coursera prog ass 2, scoping rules, uses <<-
 
-## Write a short comment describing this function
+## this funciton computes the inverse of a matrix
+## 
+#The first function, makeCacheMatrix creates a list containing a function to
+#set the value of the matrix
+#get the value of the matrix
+#set the value of the inverse of the matrix
+#get the value of the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
 
+}
+get <- function() x
+setinverse <- function(inverse) m <<- inverse
+getinverse <- function() m
+list(set = set, get = get,
+     setinverse = setinverse,
+     getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## this function checks to see if it's already been solved
+##if not it copmutes it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+    if(!is.null(m)) {
+      message("getting cached data")
+        return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
+
+#exemplar input, run the code then try this (remove hashes):
+# a <- matrix(1:4,2,2)
+# a2 <- makeCacheMatrix(a)
+# a2$get()
+# a2$getinverse()
+# cacheSolve(a2)
+# a2$getinverse()
